a991fffb739752b7f85a3d33b21e7b211b168a62
Ariel Pablo Klein <apklein@gmail.com>
2013-07-15 15:05:03 -0300

    #

--- LogicTrackerMobile/src/com/logictracker/mobileapp/MainActivity.java
+++ LogicTrackerMobile/src/com/logictracker/mobileapp/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends DroidGap {
 	}
 
 	private void setDefaultAppPreferences() {
-		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
 		Editor editor = sharedPrefs.edit();
 		editor.putString("Device.IMEI", DeviceState.getIMEI(this));
 		editor.commit();
